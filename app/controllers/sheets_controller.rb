ab02cc7e53c26b7dd12de7ec3d85d584fb3c601e
toyosikos <puroguram1155@gmail.com>
2024-09-08 15:49:02 +0900

    予約ファイルの3つの実装

---
+++ app/controllers/sheets_controller.rb
@@ -0,0 +1,5 @@
+class SheetsController < ApplicationController
+    def index
+        # Add your code here
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
+    
   # Define routes for movies (public access)
   resources :movies, only: %i[index show] 
-
+  resources :sheets
   # Admin namespace routes
   namespace :admin do
     resources :movies, only: [:index, :new, :create,:edit,:update,:show,:destroy] # admin/movies に関するルートを定義
