f8e327c5b78f45b159bec8665c3124a896c8fc47
toyosikos <puroguram1155@gmail.com>
2024-09-08 11:51:52 +0900

    検索機能

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,10 +1,16 @@
 # app/controllers/movies_controller.rb
 
 class MoviesController < ApplicationController
-    def index
-      @movies = Movie.all  # 映画のデータを取得
+  def index
+    if params[:name].present?
+      # name または description に検索キーワードが含まれる映画を検索
+      @movies = Movie.where("name LIKE ? OR description LIKE ?", "%#{params[:name]}%", "%#{params[:name]}%")
+    else
+      # 検索キーワードがなければ全ての映画を取得
+      @movies = Movie.all
       @message = "映画一覧21"  # メッセージを作成
     end
+  end   
     def show
       @movie = Movie.all # 映画のデータを取得
     end    
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,7 @@
 class Movie < ApplicationRecord
     validates :name, uniqueness: true
     validates :id, uniqueness: true
+    scope :search, -> (query) {
+        where("name LIKE ? OR description LIKE ?", "%#{query}%", "%#{query}%") if query.present?
+      }
 end
