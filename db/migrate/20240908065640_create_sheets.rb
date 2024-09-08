63566aade39e983c4a210b34e5ef30ec26e405dc
toyosikos <puroguram1155@gmail.com>
2024-09-08 15:57:49 +0900

    マイグレーション

---
+++ db/migrate/20240908065640_create_sheets.rb
@@ -0,0 +1,10 @@
+class CreateSheets < ActiveRecord::Migration[7.1]
+  def change
+    create_table :sheets do |t|
+      t.string :name
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
